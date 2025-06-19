5605854f7346e14431e346903405438ec04da899
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-06-19 23:44:18 +0600

    basic project setup

---
+++ src/pages/shared/ZapShiftLogo/ZapShiftLogo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from "../../../assets/logo.png"
+
+const ZapShiftLogo = () => {
+    return (
+        <div>
+            <img src={logo} alt="" />
+        </div>
+    );
+};
+
+export default ZapShiftLogo;
