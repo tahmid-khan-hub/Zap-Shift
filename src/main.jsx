debaa82d18622ed8dcc4e6ec3f5e1b1daf986913
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-06-20 23:49:01 +0600

    added authContext, firebase setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './router/router.jsx'
+import AuthProvider from './Contexts/AuthContext/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className='urbanist max-w-[1500px] mx-auto'>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </div>
   </StrictMode>,
 )
